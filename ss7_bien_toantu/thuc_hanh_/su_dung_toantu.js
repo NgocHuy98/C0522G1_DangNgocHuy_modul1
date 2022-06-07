b9e57715dff456ac7ae242bdfec4592951dc97aa
NgocHuy98 <huydn2101@gmail.com>
2022-06-07 23:19:52 +0700

    bai tap toan tu

---
+++ ss7_bien_toantu/thuc_hanh_/su_dung_toantu.js
@@ -0,0 +1,8 @@
+let width = 30
+let height = 20
+let area = width * height
+document.write('width='+width);
+document.write('<br/>');
+document.write('height='+height);
+document.write('<br/>');
+document.write('area='+area);
